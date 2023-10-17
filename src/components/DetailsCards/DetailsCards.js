f7ff34926215722dd9af6bad644647c4a6548ba7
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-10-17 22:43:12 +0530

    feat : task Complete

--- src/components/DetailsCards/DetailsCards.js
+++ src/components/DetailsCards/DetailsCards.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
 
-function DetailsCards() {
+function DetailsCards({ id, avatar, name, email, bankAccounts }) {
   return (
     <>
-      
+      <div
+        className="card  mt-3"
+        style={{ width: "38rem", backgroundColor: "#F7F2CB" }}
+      >
+        <div className="p-1 text-dark">
+          <div className="d-flex justify-content-evenly">
+            <div className="">
+              {" "}
+              <img
+                src={avatar}
+                alt="fsd"
+                className="mt-3"
+                style={{
+                  width: "80px",
+                  borderRadius: "50%",
+                  padding: "2px",
+                  border: "3px solid blue",
+                }}
+              />
+            </div>
+            <div>
+              <div className="">
+                <p>id :{id}</p>
+                <p>Name: {name}</p>
+                <p>email: {email}</p>
+              </div>
+            </div>
+            <div>
+              {bankAccounts.map((bankacount, i) => {
+                const { accountNumber, balance, accountType } = bankacount;
+                return (
+                  <>
+                    <h5 className="fw-bold">Acount {i + 1}</h5>
+                    <p>bank ac number {accountNumber}</p>
+                    <p>bank balane : {balance}</p>
+                    <p>bank ac type: {accountType}</p>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default DetailsCards
+export default DetailsCards;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,20 +1,58 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import DetailsCards from "../../components/DetailsCards/DetailsCards";
 
 function Home() {
-    const [bankData, setBankData] = useState({});
+  const [bankData, setBankData] = useState([]);
+  const [findName, setFindName] = useState("");
   async function getBankData() {
     let response = "";
-    response = await axios.get(
-      "https://run.mocky.io/v3/9936ff4a-7946-41d2-938c-596aa875b566"
-    );
-    console.log(response.data);
+
+    try {
+      response = await axios.get(
+        "https://run.mocky.io/v3/9936ff4a-7946-41d2-938c-596aa875b566"
+      );
+      console.log(response.data.users);
+      setBankData(response.data.users);
+    } catch (e) {
+      console.log(e);
+    }
   }
   useEffect(() => {
     getBankData();
   });
-  return <>
-  </>;
+  return (
+    <>
+      <div className="container mt-2 py-5 bg-danger-subtle">
+        <div className="container w-50">
+          <input
+            type="text"
+            className="form-control"
+            value={findName}
+            onChange={(e) => {
+              setFindName(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+      <div className="mx-auto d-flex justify-content-center align-items-center flex-column ">
+        {bankData.map((user) => {
+          const { id, avatar, name, email, password, bankAccounts } = user;
+
+          return (
+            <DetailsCards
+              id={id}
+              avatar={avatar}
+              name={name}
+              email={email}
+              password={password}
+              bankAccounts={bankAccounts}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Home;
